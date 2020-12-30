d61c183ce2749e37219f0f767f18490d104c8d9e
weipinl <wplo@ucdavis.edu>
2020-12-29 21:36:55 -0800

    12/29/2020

---
+++ Section23/facerecognitionbrain/src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30, 
+      density: {
+        enable: true, 
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* {  <FaceRecognition />
+        } */}
+      </div>
+    );
+  }
+}
+
+
+export default App;
